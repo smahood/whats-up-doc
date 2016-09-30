b017e6c15f76bb785c3fb80d19d2fbc07991bd2a
Shaun Mahood <smahood@gmail.com>
2016-09-30 14:30:53 -0600

    Initial Commit

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
+(defproject whats-up-doc "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -33,13 +34,16 @@
                 ;; the presence of a :figwheel configuration here
                 ;; will cause figwheel to inject the figwheel client
                 ;; into your build
+                :figwheel     {:on-jsload "whats-up-doc.core/mount-root"
                                ;; :open-urls will pop open your application
                                ;; in the default browser once Figwheel has
                                ;; started and complied your application.
                                ;; Comment this out once it no longer serves you.
                                :open-urls ["http://localhost:3449/index.html"]}
 
+                :compiler     {:main                 whats-up-doc.core
                                :asset-path           "js/compiled/out"
+                               :output-to            "resources/public/js/compiled/whats_up_doc.js"
                                :output-dir           "resources/public/js/compiled/out"
                                :source-map-timestamp true
                                :external-config      {:re-frisk {:enabled true}}
@@ -53,6 +57,8 @@
                ;; lein cljsbuild once min
                {:id           "min"
                 :source-paths ["src"]
+                :compiler     {:output-to     "resources/public/js/compiled/whats_up_doc.js"
+                               :main          whats-up-doc.core
                                :optimizations :advanced
                                :pretty-print  false}}]}
 
